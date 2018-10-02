afc2773592f0786246765751ead64aaafd75ccbb
CaelmBleidd <alex.menshutin99@gmail.com>
2018-10-02 15:24:37 +0300

    Just task

---
+++ Web/Homeworks/Servlets/wp3/src/main/java/ru/itmo/wp/servlet/StaticServlet.java
@@ -0,0 +1,55 @@
+package ru.itmo.wp.servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+
+public class StaticServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String uri = request.getRequestURI();
+
+        File file = new File(getServletContext().getRealPath("/static" + uri));
+
+
+
+        if (file.isFile()) {
+            response.setContentType(getContentTypeFromName(uri));
+            OutputStream outputStream = response.getOutputStream();
+            Files.copy(file.toPath(), outputStream);
+            outputStream.flush();
+        } else {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
+
+    private String getContentTypeFromName(String name) {
+        name = name.toLowerCase();
+
+        if (name.endsWith(".png")) {
+            return "image/png";
+        }
+
+        if (name.endsWith(".jpg")) {
+            return "image/jpeg";
+        }
+
+        if (name.endsWith(".html")) {
+            return "text/html";
+        }
+
+        if (name.endsWith(".css")) {
+            return "text/css";
+        }
+
+        if (name.endsWith(".js")) {
+            return "application/javascript";
+        }
+
+        throw new IllegalArgumentException("Can't find content type for '" + name + "'.");
+    }
+}

6091d83288ceb233e00f2895eed8ccd3e81b5e98
CaelmBleidd <alex.menshutin99@gmail.com>
2018-10-21 19:04:44 +0300

    Third term. First lab. Third task done

---
+++ Algorithms/Third Term/Laba 1/problem_c/main.cpp
@@ -0,0 +1,76 @@
+#include <vector>
+#include <iostream>
+#include <set>
+#include <map>
+#include <algorithm>
+
+int64_t const AMOUNT_VERTEXES = (int64_t) 1e5 + 1;
+
+std::set<uint64_t> points;
+std::map<std::pair<uint64_t, uint64_t>, uint64_t> number_of_edge;
+std::vector<std::set<int64_t>> edges;
+std::vector<uint64_t> time_in;
+std::vector<uint64_t> fup;
+std::vector<bool> used;
+
+uint64_t timer = 0;
+
+void dfs(uint64_t vertex, uint64_t parent) {
+    used[vertex] = true;
+    ++timer;
+    time_in[vertex] = timer;
+    fup[vertex] = timer;
+    int64_t amount_children{0};
+
+    for (auto i : edges[vertex]) {
+        if (i == parent)
+            continue;
+        if (used[i] == 1)
+            fup[vertex] = std::min(fup[vertex], time_in[i]);
+        else {
+            dfs(i, vertex);
+            fup[vertex] = std::min(fup[vertex], fup[i]);
+
+            if (time_in[vertex] <= fup[i] && parent != 0)
+                points.insert(vertex);
+            ++amount_children;
+        }
+
+    }
+    if (parent == 0 && amount_children > 1)
+        points.insert(vertex);
+
+}
+
+int main() {
+    uint64_t n{0}, m{0};
+    std::cin >> n >> m;
+
+    edges.resize(n + 1);
+    time_in.resize(n + 1);
+    fup.resize(n + 1);
+    used.assign(n + 1, false);
+
+    time_in[0] = INT64_MAX;
+
+
+    for (uint64_t i = 1; i <= m; ++i) {
+        uint64_t vertex_1{0}, vertex_2{0};
+        std::cin >> vertex_1 >> vertex_2;
+        number_of_edge[std::make_pair(vertex_1, vertex_2)] = i;
+        number_of_edge[std::make_pair(vertex_2, vertex_1)] = i;
+        edges[vertex_1].insert(vertex_2);
+        edges[vertex_2].insert(vertex_1);
+    }
+
+    for (uint64_t i = 1; i <= n; ++i) {
+        if (!used[i])
+            dfs(i, 0);
+    }
+
+    std::cout << points.size() << '\n';
+    for (auto i : points)
+        std::cout << i << ' ';
+
+    return 0;
+}
